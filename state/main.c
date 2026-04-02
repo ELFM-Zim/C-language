0c8fb8b4678f03b464c376ae70c410f18a4a9718
JoaoFLM <lopesmonteirojoaofelipe@gmail.com>
2026-04-02 05:22:50 -0300

    state change finished

--- state/main.c
+++ state/main.c
@@ -1,24 +1,45 @@
 #include<raylib.h>
+#include<string.h>
 
 enum SCREEN_STATE
 {
-	MOVER;
-	PINTAR;
+	MOVER,
+	PARAR
 };
 
-SCREEN_STATE estado;
+enum SCREEN_STATE estado;
 
 int main()
 {
 
 	InitWindow(1024, 720, "State");
-
-	estado = mover;
-
+	char state[6];
+	estado = MOVER;
+	float speed = 0.1;
+	Vector2 rec_pos = {100, 100};
 	while(!WindowShouldClose())
 	{
-		BeginDrawing();
 
+		if(IsKeyPressed(KEY_ONE)) estado = MOVER;
+		if(IsKeyPressed(KEY_TWO)) estado = PARAR;
+		if(estado == MOVER)
+		{
+			
+			if(IsKeyDown(KEY_UP)) rec_pos.y -= 1 * speed;
+			if(IsKeyDown(KEY_DOWN)) rec_pos.y += 1 * speed;
+			if(IsKeyDown(KEY_LEFT)) rec_pos.x -= 1 * speed;
+			if(IsKeyDown(KEY_RIGHT)) rec_pos.x += 1 * speed;
+			strcpy(state, "Mover");	
+		}
+		else if(estado == PARAR)
+		{
+			strcpy(state, "Parar");
+		}
+
+		BeginDrawing();
+		ClearBackground(PINK);
+		DrawText(state, 2, 2, 24, BLACK);
+		DrawRectangleV(rec_pos, (Vector2){30,30}, RED);
 		EndDrawing();
 	}
 
